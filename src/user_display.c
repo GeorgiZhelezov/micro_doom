e0012c993fbd373483da2b64961a2cce764a8ef3
gez <georgi.e.zhelezov@gmail.com>
2024-03-05 12:13:39 +0200

    misc messing around

--- src/user_display.c
+++ src/user_display.c
@@ -4,6 +4,7 @@
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/display/cfb.h>
 
 #include "user_test_images.h"
 
@@ -15,12 +16,16 @@ K_SEM_DEFINE(sem_display, 1, 1);
 static const struct device *display_dev         = DEVICE_DT_GET(DT_ALIAS(display_ctrl));
 static const struct gpio_dt_spec display_pin_bl = GPIO_DT_SPEC_GET(DT_NODELABEL(display_bl), gpios);
 
-uint16_t *test_image             = image_bird;
-const uint32_t test_image_len    = ARRAY_SIZE(image_troll);
+// uint16_t gpu_buff[240][135];
+// uint16_t *display_buff             = &gpu_buff[0][0];
+// const uint32_t display_buff_len    = sizeof(gpu_buff) / 2;
 
-const struct display_buffer_descriptor display_buff =
+uint16_t *display_buff             = image_bird;
+const uint32_t display_buff_len    = ARRAY_SIZE(image_bird);
+
+const struct display_buffer_descriptor display_buff_conf =
 {
-	.buf_size = test_image_len,
+	.buf_size = display_buff_len,
 	.height   = 240,
 	.width    = 135,
 	.pitch    = 1,
@@ -39,64 +44,65 @@ static void user_display_swap_bytes(uint16_t *buff, size_t len)
 
 void user_display_image_rotate(void)
 {
-	for (size_t i = 0, j = test_image_len - 1; i < j; i++, j--)
+	for (size_t i = 0, j = display_buff_len - 1; i < j; i++, j--)
 	{
-		size_t temp = test_image[i];
-		test_image[i] = test_image[j];
-		test_image[j] = temp;
+		size_t temp = display_buff[i];
+		display_buff[i] = display_buff[j];
+		display_buff[j] = temp;
 	}
 }
 
 void user_display_image_mirror()
 {
-	for (size_t i = 0; i < display_buff.height; i++)
+	for (size_t i = 0; i < display_buff_conf.height; i++)
 	{
-		for (size_t j = 0; j < display_buff.width / 2; j++)
+		for (size_t j = 0; j < display_buff_conf.width / 2; j++)
 		{
-			uint16_t temp = test_image[i * display_buff.width + j];
-			test_image[i * display_buff.width + j] = test_image[i * display_buff.width + (display_buff.width - 1 - j)];
-			test_image[i * display_buff.width + (display_buff.width - 1 - j)] = temp;
+			uint16_t temp = display_buff[i * display_buff_conf.width + j];
+			display_buff[i * display_buff_conf.width + j] = display_buff[i * display_buff_conf.width + (display_buff_conf.width - 1 - j)];
+			display_buff[i * display_buff_conf.width + (display_buff_conf.width - 1 - j)] = temp;
 		}
 
-		if (display_buff.width % 2 != 0)
+		if (display_buff_conf.width % 2 != 0)
 		{
-			test_image[i * display_buff.width + (display_buff.width / 2)] = test_image[i * display_buff.width + (display_buff.width / 2)];
+			display_buff[i * display_buff_conf.width + (display_buff_conf.width / 2)] = display_buff[i * display_buff_conf.width + (display_buff_conf.width / 2)];
 		}
 	}
 }
 
 void user_display_test_image(void)
 {
-	for (size_t row = 0; row < display_buff.height; row++)
+	static uint32_t counter = 0;
+
+	for (size_t row = 0; row < display_buff_conf.height; row++)
 	{
-		for (size_t col = 0; col < display_buff.width; col++)
+		for (size_t col = 0; col < display_buff_conf.width; col++)
 		{
 			if (row && row % 16 == 0)
 			{
-				// test_image[row * display_buff.width + col] = 0xffff;
-			} 
+				display_buff[row * display_buff_conf.width + col] = 0xffff;
+			}
 			else if (col && col % 15 == 0)
 			{
-				// test_image[row * display_buff.width + col] = 0xaaff;
+				display_buff[row * display_buff_conf.width + col] = 0xaaff;
 			}
 			else
 			{
-				// test_image[row * display_buff.width + col] = 0x0;
+				// display_buff[row * display_buff_conf.width + col] = 0x0;
 			}
 		}
 	}
 
-	static uint32_t counter = 0;
 	if (counter++ % 2 == 0)
 	{
 		user_display_image_rotate();
 	}
 	else
 	{
-		user_display_image_mirror();
+		// user_display_image_mirror();
 	}
 
-	display_write(display_dev, 0, 0, &display_buff, test_image);
+	display_write(display_dev, 0, 0, &display_buff_conf, display_buff);
 }
 
 void user_display_init(void)
@@ -119,6 +125,6 @@ void user_display_init(void)
 
 	gpio_pin_set_dt(&display_pin_bl, 1);
 
-	user_display_swap_bytes(test_image, test_image_len);
-	// memset(test_image, 0, test_image_len * sizeof(test_image[0]));
+	user_display_swap_bytes(display_buff, display_buff_len);
+	memset(display_buff, 0, display_buff_conf.buf_size * sizeof(display_buff[0]));
 }
