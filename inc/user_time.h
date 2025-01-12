22d41aec53ba04cb6013cbd9d09cf2deed34edb5
gez <georgi.e.zhelezov@gmail.com>
2025-01-12 04:12:08 +0200

    user_time.h: 	added a PERFORMANCE_PROFILE() macro

--- inc/user_time.h
+++ inc/user_time.h
@@ -3,6 +3,35 @@
 
 #include <stdint.h>
 
+#include <zephyr/posix/time.h>
+
+/**
+ * @brief Performance profiler macro. See \@details.
+ * 
+ * @param tag string to print as a tag
+ * @details Oddly enough, testing with busy_wait(1us) results in 25us printed, which is weird.
+ * 			Then again, it is stated that the clock could be skewed.
+ * 			The 20ms busy wait at the end is for the blackmagicprobe RTT to catch up.
+ */
+#define PERFORMANCE_PROFILE(tag, func)                                           \
+	do                                                                           \
+	{                                                                            \
+		char *t = tag;                                                           \
+		if (t == NULL) { t = ""; }                                               \
+		uint64_t cycles_now, cycles_after;                                       \
+		cycles_now = k_cycle_get_64();                                           \
+		func;                                                                    \
+		cycles_after = k_cycle_get_64();                                         \
+		uint64_t delta_cycles = cycles_after - cycles_now;                       \
+		uint64_t delta_ns =                                                      \
+			(delta_cycles * 1000000000ULL) / CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC; \
+		uint64_t delta_us = delta_ns / 1000ULL;                                  \
+		printk("%s %4d:%s took %4llu us (%llu ns)\r\n", t, __LINE__, __func__,   \
+			   delta_us,                                                         \
+			   delta_ns);                                                        \
+		k_busy_wait(20 * 1000);                                                  \
+	} while (0);
+
 /**
  * @brief Wrapper for getting the current time in microseconds
  * 
